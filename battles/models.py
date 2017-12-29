a8738bd5df8c1b303967e3fe30ca6e06787af21a
Maciej Pawlikowski <pawlikowski.maciek@gmail.com>
2017-12-29 15:58:50 +0100

    removed pandas requirement

--- battles/models.py
+++ battles/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from units.models import Unit
-import pandas as pd
-
+from .unit_names import names
 
 class Fights(models.Model):
 
@@ -10,9 +9,7 @@ class Fights(models.Model):
     def __str__(self):
         return self.unit.name
 
-data = pd.read_csv('h3_fight_sim/CRTRAITS.TXT', sep=',', encoding='utf-8')
-
 for monster_id in range(141):
     Fights.add_to_class(
         'vs' + str(monster_id + 1),
-        models.FloatField(verbose_name=data.values[monster_id][0]))
+        models.FloatField(verbose_name=names[monster_id]))
